98f42ef21e63f0d178ce0f1ec2079eabafed1766
Lorenzo Balugani <lorenzo.balugani@gmail.com>
2021-06-10 15:33:07 +0200

    Quality of life improvements

--- React/frontend/src/containers/chatComponents/PlayerCharacters.js
+++ React/frontend/src/containers/chatComponents/PlayerCharacters.js
@@ -1,14 +1,59 @@
 import React, {useEffect, useState} from "react";
-import Style from "./Panel.module.css";
+import Style from "./Selector.module.css";
 import CharacterWizard from "../CharacterWizard";
+import Form from "react-bootstrap/Form";
+import Row from "react-bootstrap/Row";
+import Col from "react-bootstrap/Col";
+import Select from "react-select";
+import Button from "react-bootstrap/Button";
+import {useAppContext} from "../../libs/Context";
 
 export default function PlayerCharacters({campagna}) {
 
+    const [charList, setCharList] = useState([])
+    const {userToken} = useAppContext()
+    const {address} = useAppContext()
+    const [id, setId] = useState(0)
+    const [selectInput, setSelectInput] = useState([])
+    const {uid} = useAppContext()
+
+    useEffect(() => {
+        setCharList(campagna.personaggi)
+    }, []);
+
+    useEffect(() => {
+        setSelectInput(charList.map(function (entry) {
+            return {label: entry.nome, value: entry.id}
+        }))
+    }, [charList])
+
+    function update(event) {
+        let value = event.value
+        console.debug(value)
+        if (value < 0) {
+            setId(0)
+            return
+        }
+        setId(value)
+    }
+
     return (
-        <div>
-            <div className={Style.GeneralitaPanel}>
-            {campagna.personaggi.map(character => <CharacterWizard{...character}/>)}
-            </div>
+        <div style={{marginBottom: '3vh'}}>
+            <Form.Group controlId="formSelect">
+
+                <Form.Label>Seleziona un personaggio</Form.Label>
+                <Row>
+
+                    <Col>
+                        <Select options={selectInput} name="personaggio" placeholder="..." onChange={event => {
+                            update(event)
+                        }}/>
+                    </Col>
+                    <div hidden={!id} className={Style.SheetContainer}>
+                        <CharacterWizard id={id}/>
+                    </div>
+                </Row>
+            </Form.Group>
         </div>
     );
 }
